b05e8ff5f947de4bdca02d27e0ba53662c435af6
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-09-03 15:15:20 +0200

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('ciao');
+/**
+ * Ciao ragazze e ciao ragazzi,
+Esercizio di oggi: Vue Hello
+nome repo: vue-hello
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            text: 'ciao!',
+            src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwEMChx-e1bEjYNYnxN3_Bf6otohnPimjWTA&s'
+        }
+    }
+}).mount('#vueTry');
